ec8f90fa747cf4533231c24d4e85bb0ade92f8bf
sirblackbird2 <sirblackblrd2@gmail.com>
2026-07-14 15:34:51 +0700

    Completed Module 10

---
+++ 10 - Add Looping Logic to Your Code Using the do-while and while Statements in C#/Program.cs
@@ -0,0 +1,24 @@
+﻿int heroHP = 10;
+int monsterHP = 10;
+
+Random random = new Random();
+int attack = random.Next(1, 11);
+
+while (heroHP > 0 && monsterHP > 0)
+{
+    monsterHP -= attack;
+    Console.WriteLine($"Attack = {attack}, Remaining monster HP = {monsterHP}");
+
+    if (monsterHP <= 0) 
+        break;
+    
+    attack = random.Next(1,11);
+    heroHP -= attack;
+    Console.WriteLine($"Attack = {attack}, Remaining hero HP = {heroHP}");
+}
+
+if (heroHP > 0)
+    Console.WriteLine("Hero Wins!");
+
+else 
+    Console.WriteLine("Monster Wins!");
