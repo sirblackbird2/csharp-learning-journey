72e9cac5be755c37f161b8dfeeffda6a536e5dc9
sirblackbird2 <sirblackblrd2@gmail.com>
2026-07-17 20:52:50 +0700

    Completed Module 16

---
+++ 16 - Modify the content of strings using built-in string data type methods in C#/Program.cs
@@ -0,0 +1,25 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+int quantitystart = input.IndexOf(openSpan) + openSpan.Length;
+int quantityEnd = input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantitystart;
+quantity = input.Substring(quantitystart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+output = input.Replace("&trade;", "&reg");
+
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+const string closeDiv = "</div>";
+int divEnd = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divEnd, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
