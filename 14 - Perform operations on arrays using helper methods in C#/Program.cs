e511af143aaafd3f2f2c2912d6bda57f3f272599
sirblackbird2 <sirblackblrd2@gmail.com>
2026-07-17 17:51:01 +0700

    Completed Module 14

---
+++ 14 - Perform operations on arrays using helper methods in C#/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Exercise 1 "reverse words in a sentence challenge"
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] words = pangram.Split(' ');
+
+foreach (var word in words)
+{
+    char[] characterarray = word.ToCharArray();
+    Array.Reverse(characterarray);
+    string result = String.Join("", characterarray);
+    Console.Write($"{result} ");
+}
+*/
+
+// Exercise 2 "Complete a challenge to parse a string of orders, sort the orders and tag possible errors"
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(',');
+Array.Sort(orders);
+
+foreach (var order in orders)
+{
+    if (order.Length == 4)
+    {
+        Console.WriteLine(order);
+    }
+    else
+    {
+        Console.WriteLine($"{order}\t- Error");
+    }
+}
