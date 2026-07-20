cb1a0f02f2337dc09aea25c702aad840bd4b2a27
sirblackbird2 <sirblackblrd2@gmail.com>
2026-07-20 18:24:45 +0700

    Completed Module 19

---
+++ 19 - Create C# Methods with Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    showEmail(corporate[i, 0], corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    showEmail(external[i, 0], corporate[i, 1], domain: externalDomain);
+}
+
+void showEmail(string firstname, string lastname, string domain = "contoso.com") 
+{
+    string email = firstname.Substring(0,2) + lastname;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
